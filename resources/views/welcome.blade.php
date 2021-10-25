2ad399c1d1c9a7b7284a027fb7f4bfc6e2900f47
hiroki.kimura <spe553n9@sand.ocn.ne.jp>
2021-10-25 16:03:52 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>タスクリスト</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', '登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
